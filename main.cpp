cf9852a4c4922192c7e044d84c96422b66c31b3f
fabajim <jimenfab@oregonstate.edu>
2024-09-13 12:37:28 -0700

    placed main menu in a while loop, user can play game, view past scores and clear the scores.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include<time.h>
 #include<iostream>
 
-void play_game(std::string &name, int &score);
+void play_game(int &score);
 void get_name(std::string &name);
 void add_scores(std::string &name, int &scores);
 void view_scores();
@@ -13,44 +13,53 @@ int main()
 {
 	srand(static_cast<unsigned>(time(0)));
 	std::string name;
+	std::string input;
 	int score;
-	char input[2];
-	
-
 	std::cout << "Welcom!" << std::endl;
-	std::cout << "Press 1 to play" << std::endl;
-	std::cout << "Press 2 to view scores" << std::endl;
-	std::cout << "Press 3 to clear scores" << std::endl;
-	std::cout << "Press 4 to exit" << std::endl;
-	std::cout << "Please make a selection: ";
-	std::cin.getline(input, 2);
-	std::cout << std:: endl;
+	while (input[0] != '4')
+	{
+		std::cout << "Press 1 to play" << std::endl;
+		std::cout << "Press 2 to view scores" << std::endl;
+		std::cout << "Press 3 to clear scores" << std::endl;
+		std::cout << "Press 4 to exit" << std::endl;
+		std::cout << "Please make a selection: ";
+		std::cin >> input;
+		std::cout << std::endl;
+		if (input[0] == '1')
+		{
+			get_name(name);
+			play_game(score);
+			add_scores(name, score);
+		}
+		else if (input[0] == '2')
+		{
+			view_scores();
+		}
+		else if (input[0] == '3')
+		{
+			clear_scores();
+		}
+		else if (input[0] == '4')
+		{
+			break;
+		}
+		else
+		{
+			std::cout << "[Error] " << input << " Was An Invalid Entry\n" << std::endl;
+		}
+	}
 
-	if (input[0] == '1')
-	{
-		play_game(name, score);
-	}
-	else if (input[0] == '2')
-	{
-		view_scores();
-	}
-	else if (input[0] == '3')
-	{
-		clear_scores();
-	}
-
-	std::cout << "Thank You!" << std::endl;
+	std::cout << "Goodbye!" << std::endl;
 
 	return 0;
 }
 
-void play_game(std::string &name, int &score)
+void play_game(int &score)
 {
-	get_name(name);
-	std::cout << "GOOD LUCK " << name << std::endl;
+	std::cout << "GOOD LUCK " << std::endl;
 	Game game;
 	score = game.run();
-	add_scores(name, score);
+	std::cout << std::endl;
 }
 
 void get_name(std::string& name)
@@ -88,23 +97,24 @@ void view_scores()
 		}
 		fio.close();
 	}
+	std::cout << std::endl;
 }
 
 void clear_scores()
 {
-	char verify[2];
+	std::string varify;
 	std::cout << "Are you sure you want to clear the scores? Action cannot be undon! (y/n): ";
-	std::cin.getline(verify, 2);
-	if (verify[0] == 'Y' || verify[0] == 'y')
+	std::cin >> varify;
+	if (varify[0] == 'Y' || varify[0] == 'y')
 	{
 		empty_scores();
+		std::cout << "Scores Cleared\n" << std::endl;
 	}
 
 }
 
 void empty_scores()
 {
-	std::cout << "In empty" << std::endl;
 	std::ofstream f; 
 	f.open("scores.txt", std::ofstream::out | std::ofstream::trunc);
 	f << "";
