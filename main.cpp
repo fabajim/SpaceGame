db5db6e0d8e19920c5f46edec724ec593888f8b8
fabajim <jimenfab@oregonstate.edu>
2024-09-17 10:40:42 -0700

    updated string comparison in main added scores.txt that keeps trak of player scores.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
 	std::string input;
 	int score;
 	std::cout << "Welcom!" << std::endl;
-	while (input[0] != '4')
+	while (true)
 	{
 		std::cout << "Press 1 to play" << std::endl;
 		std::cout << "Press 2 to view scores" << std::endl;
@@ -25,21 +25,21 @@ int main()
 		std::cout << "Please make a selection: ";
 		std::cin >> input;
 		std::cout << std::endl;
-		if (input[0] == '1')
+		if (input.compare("1") == 0)
 		{
 			get_name(name);
 			play_game(score);
 			add_scores(name, score);
 		}
-		else if (input[0] == '2')
+		else if (input.compare("2") == 0)
 		{
 			view_scores();
 		}
-		else if (input[0] == '3')
+		else if (input.compare("3") == 0)
 		{
 			clear_scores();
 		}
-		else if (input[0] == '4')
+		else if (input.compare("4") == 0)
 		{
 			break;
 		}
@@ -72,11 +72,11 @@ void add_scores(std::string& name, int& score)
 {
 	std::string s_score;
 	s_score = std::to_string(score);
-	std::ofstream fio("scores.txt", std::ios::app);
-	if (fio.is_open())
+	std::ofstream f("scores.txt", std::ios::app);
+	if (f.is_open())
 	{
-		fio << name << " " << s_score << std::endl;
-		fio.close();
+		f << name << " " << s_score << std::endl;
+		f.close();
 	}
 	else
 	{
@@ -87,15 +87,15 @@ void add_scores(std::string& name, int& score)
 void view_scores()
 {
 	std::string line;
-	std::ifstream fio("scores.txt");
-	if (fio.is_open())
+	std::ifstream f("scores.txt");
+	if (f.is_open())
 	{
 		std::cout << "SCORES" << std::endl;
-		while (getline(fio, line))
+		while (getline(f, line))
 		{
 			std::cout << line << std::endl;
 		}
-		fio.close();
+		f.close();
 	}
 	std::cout << std::endl;
 }
@@ -103,7 +103,7 @@ void view_scores()
 void clear_scores()
 {
 	std::string varify;
-	std::cout << "Are you sure you want to clear the scores? Action cannot be undon! (y/n): ";
+	std::cout << "Are you sure you want to clear the scores? Action cannot be undone! (y/n): ";
 	std::cin >> varify;
 	if (varify[0] == 'Y' || varify[0] == 'y')
 	{
